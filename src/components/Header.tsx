aa945a03d9e60938209e0acb0743c3fbe4524b2a
Bharat Yadav <yadavbharat386@gmail.com>
2024-12-06 20:11:53 +0530

    Made responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,25 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "@/store/store";
 
+import { useIsMobile } from "@/hooks/use-mobile";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
 function Header() {
   const { pageName } = useSelector((state: RootState) => state.general);
 
+  const isMobile = useIsMobile();
+
   return (
     <div className="flex justify-between items-center h-[3rem] my-2 w-full">
-      <div className="text-[1.2rem] uppercase">{pageName}</div>
+      <div className="text-[1.2rem] uppercase flex gap-4">
+        {isMobile && (
+          <div>
+            {" "}
+            <SidebarTrigger />{" "}
+          </div>
+        )}
+        <div>{pageName} </div>
+      </div>
       <div></div>
     </div>
   );
--- src/pages/content-pages/HeroSection.tsx
+++ src/pages/content-pages/HeroSection.tsx
@@ -37,8 +37,8 @@ function HeroSection() {
 
   return (
     <div className="bg-[hsl(var(--border));] pt-8 pb-10 px-6 rounded-sm flex flex-col gap-5">
-      <div className="flex w-full">
-        <div className="w-1/4 font-medium">Caption</div>
+      <div className="flex w-full lg:flex-row flex-col lg:gap-0 gap-2">
+        <div className="lg:w-[17.3rem] w-auto  font-medium">Caption</div>
         <div className="grow max-w-[47rem]">
           <CustomInput
             maxChars={30}
@@ -49,8 +49,8 @@ function HeroSection() {
         </div>
       </div>
 
-      <div className="flex w-full">
-        <div className="w-1/4 font-medium ">Description</div>
+      <div className="flex w-full lg:flex-row flex-col lg:gap-0 gap-2">
+        <div className="lg:w-[17.3rem] w-auto font-medium ">Description</div>
         <div className="grow max-w-[47rem] flex flex-col">
           <CustomTextArea
             maxChars={50}
@@ -58,11 +58,12 @@ function HeroSection() {
             setText={setDescription}
             className="h-32"
           />
-          <PurpleButton className="mt-4" handleClick={handleSubmit}>
-            Save
-          </PurpleButton>
         </div>
       </div>
+
+      <PurpleButton className="mt-2 lg:ml-[17.3rem]" handleClick={handleSubmit}>
+        Save
+      </PurpleButton>
     </div>
   );
 }
